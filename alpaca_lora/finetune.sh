a75931eec4bb4a50728bd71dde70fe915d9321fa
DVampire <1271421361@qq.com>
2023-04-15 00:11:49 +0800

    add project

---
+++ alpaca_lora/finetune.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0 \
+torchrun --nproc_per_node=1 \
+finetune.py \
+    --base_model 'decapoda-research/llama-7b-hf' \
+    --data_path 'datasets/processed_data/alpaca_data_cleaned.json' \
+    --output_dir './workspace/exp1'
